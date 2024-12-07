0d9e107bb78230ed61edd52bc9d0b57184760c88
24HP1005 24HP1005 <24HP1005@admins-iMac.local>
2024-12-06 21:16:55 -0800

    nn

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+
+import imageToAdd from "./../assets/images/logo.png";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import { Bookmark } from "lucide-react";
+import { Heart } from "lucide-react";
+import { MessageCircle } from "lucide-react";
+import { Send } from "lucide-react";
+
+import { useState, useEffect } from "react";
+
+type likeTypes = {
+  postlikeImage: string;
+  username: string;
+  _id: string;
+};
+
+type postType = {
+  _id: string;
+  caption: string;
+  postImage: string;
+  profileImage: string;
+  userId: string;
+  likes: likeTypes[];
+}[];
+
+const Page = () => {
+  const [posts, setPosts] = useState<postType>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const getPosts = async () => {
+    console.log("working");
+    const jsonData = await fetch("https://ig-server-v2.onrender.com/posts");
+    const response = await jsonData.json();
+    setPosts(response);
+    setLoading(false);
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+  if (loading) return "loading";
+
+  return (
+    <div>
+      {posts?.map((post) => {
+        return (
+          <Card key={post._id} className="w-fit">
+            <div></div>
+            <CardHeader>{/* <div>{post.userId.username}</div> */}</CardHeader>
+            <CardContent>
+              <img src={post.profileImage} width="340px" height="600px" />
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <div className="flex space-y-0">
+                <Heart />
+                <MessageCircle />
+                <Send />
+              </div>
+              <Bookmark />
+            </CardFooter>
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Page;
